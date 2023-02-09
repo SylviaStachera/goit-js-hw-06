5d45e20b0100a32f72b9045f4ef50b714983c2af
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-09 18:40:07 +0100

    Update task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+const listIngredients = document.querySelector('#ingredients')
+
+// 1. Utworzy oddzielny element <li>. Koniecznie użyj metody document.createElement().
+// 2. Doda nazwę elementu jako jego zawartość tekstową.
+// 3. Doda do elementu klasę item.
+// 4. Po czym umieści wszystkie <li> na liście podczas jednej operacji ul#ingredients.
+
+;[...ingredients].forEach(item => {
+	const itemIngedient = document.createElement('li')
+	itemIngedient.textContent = item
+	listIngredients.append(itemIngedient)
+	itemIngedient.classList.add('item')
+})
+
+//Sprawdzenie
+//console.log(listIngredients);
