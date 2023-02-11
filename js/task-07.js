1de94086d49ea229fb65b280beb873d23c1f4c65
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-11 15:43:32 +0100

    Update task 07;

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const fontSizeControle = document.querySelector('#font-size-control')
+const textChange = document.querySelector('#text')
+
+fontSizeControle.addEventListener('input', event => {
+	//Bieżąca wartość input
+	//console.log(event.currentTarget.value);
+	let currentValue = event.currentTarget.value
+	textChange.style.fontSize = currentValue + 'px'
+})
