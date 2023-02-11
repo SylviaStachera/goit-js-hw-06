34d002fae51f14547971ab1392b061aef0f20dea
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-11 15:23:38 +0100

    Update task 06

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,10 @@ const textInput = document.querySelector('#validation-input')
 //Dostęp do atrybutu data-length
 //console.log(textInput.dataset.length);
 
-textInput.addEventListener('blur', event => {
-	//Długość wpisanego tekstu
-	//console.log(event.currentTarget.value.length);
+//Długość wpisanego tekstu
+//console.log(event.currentTarget.value.length);
 
+textInput.addEventListener('blur', event => {
 	if (event.currentTarget.value.length < textInput.dataset.length) {
 		textInput.classList.remove('valid')
 		textInput.classList.add('invalid')
