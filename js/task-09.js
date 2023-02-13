0a07c0acbc5c8bddb319ba2a3d26d756105edf6d
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-13 19:10:45 +0100

    Update task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+const changeColor = document.querySelector('.change-color')
+const changeBody = document.querySelector('body')
+
+changeColor.addEventListener('click', event => {
+	changeBody.style.backgroundColor = getRandomHexColor()
+})
