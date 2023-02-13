e228e509a63fbafe3f748067953337553e5ad5db
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-13 20:45:02 +0100

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+const inputNumber = document.querySelector('#controls input[type="number"]')
+const buttonCreate = document.querySelector('#controls button[data-create]')
+const buttonDestroy = document.querySelector('#controls button[data-destroy]')
+const containerBoxes = document.querySelector('#boxes')
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
+
+//Tworzenie boxów
+
+const createBoxes = amount => {
+	//tworzę pusty arry aby dodać tam divy, każdy z divów będzie wyglądał tak jak w pętli for w zależności od i
+	const boxToAdd = []
+
+	for (let i = 0; i <= amount; i++) {
+		const divItem = document.createElement('div')
+		divItem.style.height = `${30 + 10 * i}px`
+		divItem.style.width = `${30 + 10 * i}px`
+		divItem.style.backgroundColor = getRandomHexColor()
+		boxToAdd.push(divItem)
+	}
+	//console.log(boxToAdd);
+	return boxToAdd
+}
+
+//Usuwanie boxów
+const destroyBoxes = () => {
+	containerBoxes.innerHTML = ''
+	inputNumber.value = null
+}
+
+//Zadarzenia na przyciskach
+buttonCreate.addEventListener('click', () => {
+	let countBoxes = createBoxes(inputNumber.value)
+
+	containerBoxes.append(...countBoxes)
+})
+
+buttonDestroy.addEventListener('click', destroyBoxes)
