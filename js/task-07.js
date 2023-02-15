2574b9ab8efbfac975528c6300df64cba8e6fa04
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-15 18:16:05 +0100

    Update tasl 07. Add set attribute

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontSizeControle = document.querySelector('#font-size-control')
 const textChange = document.querySelector('#text')
 
+fontSizeControle.setAttribute('value', '16')
+
 fontSizeControle.addEventListener('input', event => {
 	//Bieżąca wartość input
 	//console.log(event.currentTarget.value);
