54d14b690df17d4299e81c30d5604dbb78d0baca
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-17 21:18:57 +0100

    Update task 10 - finish

--- js/task-10.js
+++ js/task-10.js
@@ -3,24 +3,31 @@ const buttonCreate = document.querySelector('#controls button[data-create]')
 const buttonDestroy = document.querySelector('#controls button[data-destroy]')
 const containerBoxes = document.querySelector('#boxes')
 
+//==============================
+
 function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`
 }
 
-//Tworzenie boxów
+//Tworzenie boxów===============
+let boxToAdd = []
+let boxesCount = 0;
+
 
 const createBoxes = amount => {
 	//tworzę pusty arry aby dodać tam divy, każdy z divów będzie wyglądał tak jak w pętli for w zależności od i
-	const boxToAdd = []
 
-	for (let i = 0; i < amount; i++) {
+	//for ( boxesCount; boxesCount < amount; boxesCount++) {
+	for ( let i = 0; i < amount; i++) {
 		const divItem = document.createElement('div')
-		divItem.style.height = `${30 + 10 * i}px`
-		divItem.style.width = `${30 + 10 * i}px`
+		divItem.style.height = `${30 + 10 * boxesCount}px`
+		divItem.style.width = `${30 + 10 * boxesCount}px`
 		divItem.style.backgroundColor = getRandomHexColor()
 		boxToAdd.push(divItem)
+		boxesCount++
 	}
 	//console.log(boxToAdd);
+	//console.log(boxesCount);
 	return boxToAdd
 }
 
@@ -28,16 +35,19 @@ const createBoxes = amount => {
 const destroyBoxes = () => {
 	containerBoxes.innerHTML = ''
 	inputNumber.value = null
+	//boxesCount = 0;
+	//boxToAdd = [];
 }
 
-//Zadarzenia na przyciskach
+//Zadarzenia na przyciskach===============
 buttonCreate.addEventListener('click', () => {
-	let countBoxes = createBoxes(inputNumber.value)
-	containerBoxes.append(...countBoxes)
-	if (containerBoxes.children !== []) {
-		containerBoxes.innerHTML = ''
-		containerBoxes.append(...countBoxes)
-	}
+	let addBoxes = createBoxes(inputNumber.value)
+	containerBoxes.append(...addBoxes)
+	//Wersja z warunkiem, żeby kasować dodane divy po kliknięciu create ponownie
+	// if (containerBoxes.children !== []) {
+	// 	containerBoxes.innerHTML = ''
+	// 	containerBoxes.append(...countBoxes)
+	// }
 	//console.log(containerBoxes.children);
 })
 
